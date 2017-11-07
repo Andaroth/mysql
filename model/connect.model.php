b14ed636de1c587c5253743b7ab1ab2e0db0b9ec
Anda <ax.fiolle@gmail.com>
2017-11-07 21:05:27 +0100

    commit to house

--- model/connect.model.php
+++ model/connect.model.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $database = new PDO("mysql:host=".$dbhost.";dbname=".$dbname.";charset=utf8", $dbuser, $dbpass);
-} catch(Exception $e) {echo("DB id are wrong");die();}
+    $db = new PDO("mysql:host=".$dbhost.";dbname=".$dbname.";charset=utf8", $dbuser, $dbpass);
+} catch(Exception $e) {echo("DB conf is like your life. It's wrong.");die();}
 
 ?>
--- views/blogroll.views.php
+++ views/blogroll.views.php
@@ -1,11 +1,25 @@
 <?php
-if ($logged) { ?>
+// if ($logged) { ?>
 <h1>Anda Private Blog</h1>
     <p>Page privée des news</p>
 <?php 
-// 
-?>
-<?php } else { ?>
-<h1>Anda Private Blog</h1>
-    <p>Accès interdit</p>
-<?php } ?>
+// get all the news
+foreach ($getNews as $thisQuery) {
+    $n_title = htmlspecialchars($thisQuery["title"]);
+    $n_content = htmlspecialchars($thisQuery["content"]);
+    $n_date = $thisQuery["date"];
+    echo 
+"    <article class=\"news\">
+".
+"        <h2>".$n_title."</h2>
+".
+"        <p>".$n_content."</p>
+".
+"        <div class=\"date\">".$n_date."</div>
+".
+"    </article>
+";
+    
+} /* else {
+include("./views/homepage.views.php);
+*/ ?>
